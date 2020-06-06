f7b065d04f644ea4eea64c4bdeb9eeb9e3c822ae
Asato TAKAHASHI <asato730@gmail.com>
2020-06-06 19:49:26 +0900

    move to edit article page when click on article card on reading later list page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ Capybara.register_driver :remote_chrome do |app|
         "no-sandbox",
         "headless",
         "disable-gpu",
-        "window-size=1440,900"
+        "window-size=375,812"
       ]
     }
   )
