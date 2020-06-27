91e9db84f1f26947439dd673ede424b50022bfc0
Asato TAKAHASHI <asato730@gmail.com>
2020-06-27 22:15:06 +0900

    ネットサーフィン

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -5,7 +5,7 @@ module MetaTagsHelper
       # title: "Learticle",
       reverse: true,
       charset: "utf-8",
-      description: "LearticleはWebサーフィンを学びの時間に変えるブックマークサービスです。スキマ時間に出会った記事をさくっとリストに登録して、あとで読んだらメモをつけて学びとして管理して、その学びをSNSでアウトプットすることで学びの定着や新たな気づきを得ることができます。",
+      description: "Learticleはネットサーフィンを学びの時間に変えるブックマークサービスです。スキマ時間に出会った記事をさくっとリストに登録して、あとで読んだらメモをつけて学びとして管理して、その学びをSNSでアウトプットすることで学びの定着や新たな気づきを得ることができます。",
       canonical: request.original_url,
       og: {
         site_name: :site,
